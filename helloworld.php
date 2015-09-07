cc29f995e51ca334c56183f542f744c360bf014b
Katariina Linde <klinde@greeny.cs.tlu.ee>
2015-09-07 13:46:35 +0300

    age

--- helloworld.php
+++ helloworld.php
@@ -1,8 +1,23 @@
 <?php
 
-$first_name = "Katariina";
-$last_name = "Linde";
+	$first_name = "Katariina";
+	$last_name = "Linde";
 
-echo $first_name." ".$last_name; //." ". liitmine
-//kommentaarid
-?> 
+	echo $first_name." ".$last_name; //." ". liitmine
+
+?>
+
+<br>
+
+<?php
+
+	$age = 21;
+	
+	//if(loogikatehe){tõene} else {väär}
+	if($age>=18){
+		echo "Oled täisealine";
+	} else{
+		echo "Oled alaealine";
+	}
+
+?>
